fb2cdaec57bf4b6a23e83db84fdf62091d2a132b
Chayatip-Chaivirat <chaivirat.chayatip@gmail.com>
2025-11-06 15:23:29 +0100

    Create Tile.cs and Map.cs
    Map.cs isn't done

---
+++ PacMan/Map.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PacMan
+{
+    internal class Map
+    {
+        private List<string> result;
+        public static Tile[,] tileArray;
+
+        public Map(string fileName)
+        {
+
+        }
+
+        public List<string> ReadFromFile(string fileName)
+        {
+            StreamReader sr = new StreamReader(fileName);
+            List<string> result = new List<string>();
+
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                result.Add(line);
+            }
+            sr.Close();
+            return result;
+        }
+
+        public void CreateMap(string fileName)
+        {
+            List<string> map = ReadFromFile(fileName);
+            tileArray = new Tile[map[0].Length, map.Count];
+
+            // 2 for loops to fill the array
+        }
+    }
+}
---
+++ PacMan/Tile.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace PacMan
+{
+    internal class Tile
+    {
+        public bool isWalkable;
+        private Texture2D tileTex;
+        private Vector2 position;
+        private Rectangle srcRec;
+        public Tile(Texture2D tileTex, Vector2 position, bool isWalkable, Rectangle srcRec)
+        {
+            this.tileTex = tileTex;
+            this.position = position;
+            this.isWalkable = isWalkable;
+            this.srcRec = srcRec;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(tileTex, position, srcRec, Color.White);
+        }
+    }
+}
