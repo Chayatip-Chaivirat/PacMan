2330c44eeb9ec224c3ba52fefff3479bbc05e066
Chayatip-Chaivirat <chaivirat.chayatip@gmail.com>
2025-11-12 11:26:51 +0100

    CollidableObject and Moveable classes

---
+++ PacMan/CollidableObject.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PacMan
+{
+    internal class CollidableObject
+    {
+        protected Texture2D tex;
+        protected Vector2 pos;
+        protected Rectangle hitBoxLive;
+    }
+}
---
+++ PacMan/Moveable.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Timers;
+using SharpDX.Direct3D11;
+using SharpDX.Direct3D9;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PacMan
+{
+    internal class Moveable : CollidableObject
+    {
+        protected int frame;
+        protected int totalFrame;
+        protected Vector2 frameSize;
+        protected double frameTimer = 100, frameInterval = 100;
+        protected Rectangle srcRec;
+        protected SpriteEffects animationFX = SpriteEffects.None;
+        protected float rotation = 0;
+        protected float scale = 1;
+
+        public Moveable(int totalFrame, Rectangle srcRec, Vector2 frameSize)
+        {
+            this.totalFrame = totalFrame;
+            this.srcRec = srcRec;
+            this.frameSize = frameSize;
+        }
+
+        public void Animation(GameTime gameTime)
+        {
+            frameTimer -= gameTime.ElapsedGameTime.TotalMilliseconds;
+            if (frameTimer <= 0)
+            {
+                frameTimer = frameInterval; frame++;
+                srcRec.X = (frame % totalFrame) * (int)frameSize.X;
+            }
+        }
+        public void AnimationSpriteEffect(GameTime gameTime)
+        {
+            if (Keyboard.GetState().IsKeyDown(Keys.Right) || Keyboard.GetState().IsKeyDown(Keys.D))
+            {
+                animationFX = SpriteEffects.None;
+                rotation = 0;
+                frameTimer -= gameTime.ElapsedGameTime.TotalMilliseconds;
+                rotation = MathHelper.ToRadians(0);
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Left) || Keyboard.GetState().IsKeyDown(Keys.A))
+            {
+                animationFX = SpriteEffects.FlipHorizontally;
+                rotation = 0;
+                frameTimer -= gameTime.ElapsedGameTime.TotalMilliseconds;
+                rotation = MathHelper.ToRadians(0);
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Up) || Keyboard.GetState().IsKeyDown(Keys.W))
+            {
+                rotation = MathHelper.ToRadians(-90);
+                frameTimer -= gameTime.ElapsedGameTime.TotalMilliseconds;
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Down) || Keyboard.GetState().IsKeyDown(Keys.S))
+            {
+                rotation = MathHelper.ToRadians(-90);
+                frameTimer -= gameTime.ElapsedGameTime.TotalMilliseconds;
+            }
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            if (tex != null)
+            {
+                spriteBatch.Draw(tex, pos, srcRec, Color.White, rotation, new Vector2(), scale, animationFX, 1);
+            }
+
+        }
+    }
+}
